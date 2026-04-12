2bf18e8df37c0bd11aabeb7db16852636c5b0be4
Ritesh Prabhu <80973725+Ritesh2800@users.noreply.github.com>
2026-04-12 23:48:59 +0530

    Update Assignment6.c

--- FOP Assignments/Assignment6.c
+++ FOP Assignments/Assignment6.c
@@ -1,25 +1,34 @@
 #include <stdio.h>
 
+// WITHOUT Recursion (Iterative)
 long int factorialIterative(int n) {
-    long int res = 1;
-    for (int i = 1; i <= n; i++) res *= i;
-    return res;
+    long int fact = 1;
+    int i;
+    for (i = 1; i <= n; i++) {
+        fact = fact * i;
+    }
+    return fact;
 }
 
+// WITH Recursion
 long int factorialRecursive(int n) {
-    if (n <= 1) return 1;
-    return n * factorialRecursive(n - 1);
+    if (n == 0 || n == 1)   // Base case
+        return 1;
+    return n * factorialRecursive(n - 1);  // Recursive call
 }
 
 int main() {
     int num;
+
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    if (num < 0) return 1;
-
-    printf("Iterative: %ld\n", factorialIterative(num));
-    printf("Recursive: %ld\n", factorialRecursive(num));
+    if (num < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        printf("\nFactorial without recursion = %ld\n", factorialIterative(num));
+        printf("Factorial with recursion    = %ld\n", factorialRecursive(num));
+    }
 
     return 0;
 }

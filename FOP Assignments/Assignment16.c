ed3097bea6e28e934db1b01e589a65406a76b06b
Ritesh Prabhu <80973725+Ritesh2800@users.noreply.github.com>
2026-04-20 00:49:17 +0530

    Update Assignment16.c

--- FOP Assignments/Assignment16.c
+++ FOP Assignments/Assignment16.c
@@ -1,19 +1,34 @@
 #include <stdio.h>
 
 int main() {
-    int n, arr[100], e[100], o[100], ec=0, oc=0;
-    printf("Enter count: ");
+    int n, i;
+
+    printf("Enter number of elements: ");
     scanf("%d", &n);
 
-    for (int i = 0; i < n; i++) {
+    int arr[n], even[n], odd[n];
+    int e = 0, o = 0;
+
+    printf("Enter %d integers:\n", n);
+    for(i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
-        if (arr[i] % 2 == 0) e[ec++] = arr[i];
-        else o[oc++] = arr[i];
+
+        if(arr[i] % 2 == 0) {
+            even[e++] = arr[i];
+        } else {
+            odd[o++] = arr[i];
+        }
     }
 
-    printf("Even numbers are: ");
-    for (int i = 0; i < ec; i++) printf("%d ", e[i]);
-    printf("\nOdd numbers are: ");
-    for (int i = 0; i < oc; i++) printf("%d ", o[i]);
+    printf("\nEven numbers:\n");
+    for(i = 0; i < e; i++) {
+        printf("%d ", even[i]);
+    }
+
+    printf("\nOdd numbers:\n");
+    for(i = 0; i < o; i++) {
+        printf("%d ", odd[i]);
+    }
+
     return 0;
 }

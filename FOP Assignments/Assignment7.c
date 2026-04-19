c3507e6f16f47cfbe2fdf292e26cc7a4c0f9940f
Ritesh Prabhu <80973725+Ritesh2800@users.noreply.github.com>
2026-04-20 01:11:38 +0530

    Update Assignment7.c

--- FOP Assignments/Assignment7.c
+++ FOP Assignments/Assignment7.c
@@ -16,22 +16,33 @@ int main() {
     scanf("%d", &n);
 
     for (i = 0; i < n; i++) {
+        printf("\n--- Student %d ---\n", i + 1);
+
         printf("Roll Number: ");
         scanf("%d", &s[i].roll);
+
         printf("Name: ");
         scanf("%s", s[i].name);
-        printf("Marks in 3 subjects: ");
+
         s[i].total = 0;
+
+        printf("Enter marks in 3 subjects:\n");
         for (j = 0; j < 3; j++) {
             scanf("%d", &s[i].marks[j]);
             s[i].total += s[i].marks[j];
         }
+
         s[i].perc = s[i].total / 3.0;
     }
 
+    printf("\n===== Student Details =====\n");
+
     for (i = 0; i < n; i++) {
-        printf("\nRoll No: %d\nName: %s\nTotal: %d\nPercentage: %.2f%%\n", 
-               s[i].roll, s[i].name, s[i].total, s[i].perc);
+        printf("\nRoll No: %d", s[i].roll);
+        printf("\nName: %s", s[i].name);
+        printf("\nTotal: %d", s[i].total);
+        printf("\nPercentage: %.2f%%\n", s[i].perc);
     }
+
     return 0;
 }
